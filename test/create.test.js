32dcde253346e30fd896d6a8b7ec48d88687db0e
MikuxII <mickaelpoupion@gmail.com>
2023-06-26 09:52:35 +0200

    file reorganisation

--- test/create.test.js
+++ test/create.test.js
@@ -10,15 +10,27 @@ beforeAll(async () => {
 });
 
 describe('Librairie', () => {
-  describe('Creation', () => {
-    test('Success', async () => {
-      const res = await axios.post(url('/livre'), livreSeeds.isbn10)
-      expect(res.status).toBe(200)
-      expect(res.data).toBe(message.succesInsert);
-      const newLivre = await Livre.findOne({ where: { isbn: livreSeeds.isbn10.isbn } });
-      expect(newLivre).toMatchObject(livreSeeds.isbn10);
-    })
+  test('Success', async () => {
+    const res = await axios.post(url('/livre'), livreSeeds.isbn10)
+    expect(res.status).toBe(200)
+    expect(res.data).toBe(message.succesInsert);
+    const newLivre = await Livre.findOne({ where: { isbn: livreSeeds.isbn10.isbn } });
+    expect(newLivre).toMatchObject(livreSeeds.isbn10);
+  })
 
+  describe('Error', () => {
+    test('Insertion to DB was failed', async () => {
+      try {
+        await axios.post(url('/livre'), livreSeeds.isbn10)
+      } catch (error) {
+        expect(error.response.status).toBe(400);      
+        expect(error.response.data).toBeTruthy()
+        expect(error.response.data.errors[0].msg).toEqual(message.invalidType)
+      }
+    })
+  })
+
+  describe('Validator', () => {
     test('Validator isbn body type', async () => {
       try {
         await axios.post(url('/livre'), {...livreSeeds.isbn10, isbn: 123 })
@@ -35,16 +47,6 @@ describe('Librairie', () => {
       } catch (error) {
         expect(error.response.data).toBeTruthy()
         expect(error.response.status).toBe(400);      
-        expect(error.response.data.errors[0].msg).toEqual(message.invalidType)
-      }
-    })
-
-    test('Insertion to DB was failed', async () => {
-      try {
-        await axios.post(url('/livre'), livreSeeds.isbn10)
-      } catch (error) {
-        expect(error.response.status).toBe(400);      
-        expect(error.response.data).toBeTruthy()
         expect(error.response.data.errors[0].msg).toEqual(message.invalidType)
       }
     })
