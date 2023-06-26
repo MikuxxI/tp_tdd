b45a790d008e4bfe8641a89709bc21da348428a6
MikuxII <mickaelpoupion@gmail.com>
2023-06-26 15:55:26 +0200

    GetOne book => test

--- test/getOne.test.js
+++ test/getOne.test.js
@@ -1,6 +1,22 @@
+const axios = require('axios');
+const livreSeeds = require('./seeders/livres.seeds.js');
+const Livre = require("../models/Livre");
+
+const url = (url) => `http://127.0.0.1:3000${url}`;
+beforeEach(async () => {
+  await Livre.destroy({ where: {} });
+});
+
 describe('Fonctionnement de l\'API', () => {
   // describe('test Erreur', ()=> {
   //   expect(1).toBe(2);
   // })
 
+  test('récupère un Livre', async ()=> {
+    const livre = await Livre.create(livreSeeds.isbn10);
+    console.debug(livre.dataValues.id)
+    const res = await axios.get(url(`/livre?${livre.dataValues.id}`));
+
+    expect(res.data).toMatchObject([livreSeeds.isbn10]);
+  })
 })
