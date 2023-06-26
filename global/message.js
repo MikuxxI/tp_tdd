ff63c927b047134588dce53bbfea6a61fe53b6e3
MikuxII <mickaelpoupion@gmail.com>
2023-06-26 16:41:08 +0200

    Error getOne => test

--- global/message.js
+++ global/message.js
@@ -1,5 +1,6 @@
 module.exports = {
     succesInsert: 'Successfully inserted',
     invalidType: 'Invalid value',
-    errorInsertion: 'Error during database insertion'
+    errorInsertion: 'Error during database insertion',
+    notFound: 'Not Found'
 };
--- test/getOne.test.js
+++ test/getOne.test.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const livreSeeds = require('./seeders/livres.seeds.js');
 const Livre = require("../models/Livre");
+const message = require('../global/message.js');
 
 const url = (url) => `http://127.0.0.1:3000${url}`;
 beforeEach(async () => {
@@ -14,9 +15,16 @@ describe('Fonctionnement de l\'API', () => {
 
   test('récupère un Livre', async ()=> {
     const livre = await Livre.create(livreSeeds.isbn10);
-    console.debug(livre.dataValues.id)
-    const res = await axios.get(url(`/livre?${livre.dataValues.id}`));
-
+    const res = await axios.get(url(`/livre?id=${livre.dataValues.id}`));
     expect(res.data).toMatchObject([livreSeeds.isbn10]);
   })
+
+  test('Ne trouve pas le livre', async () => {
+    try {
+      await axios.get(url('/livres?id=1'));
+    } catch (error) {
+      expect(error.response.status).toBe(404);
+      expect(error.response.statusText).toBe(message.notFound);
+    }
+  });
 })
